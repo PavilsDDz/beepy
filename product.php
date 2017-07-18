5bb4e62d3e03513d19941bbfb8a2ba7557042b67
PavilsDDz <pavilsdzi@gmail.com>
2017-07-18 13:53:27 +0300

    Header update, working languages in product.php

--- product.php
+++ product.php
@@ -7,7 +7,7 @@
     if(isset($_GET['id']) && $_GET['id'] > 0){
         $id = $_GET['id'];
     } else{
-        header("location: ".$SiteUrl."searching.php");
+       header("location: ".$SiteUrl."searching.php");
     }
   
 
